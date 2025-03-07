039411e083f528dbb63c115415404e8fa0297d91
DiegoNT1 <diegoagustinhomelo@gmail.com>
2025-03-07 17:28:42 -0300

    first commit

---
+++ dist/alunos.js
@@ -0,0 +1,20 @@
+const alunos = [{
+  nome: 'Paula',
+  nota: 10
+}, {
+  nome: 'Bruno',
+  nota: 6
+}, {
+  nome: 'Letiacia',
+  nota: 2
+}, {
+  nome: 'Carlos',
+  nota: 4
+}, {
+  nome: 'Lucas',
+  nota: 7
+}];
+const aprovados = alunos.filter(function (item) {
+  return item.nota >= 6;
+});
+console.log(aprovados);
