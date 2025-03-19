900d3728e9dc73180463096eae2a7545e24a6c49
DiegoNT1 <diegoagustinhomelo@gmail.com>
2025-03-19 20:18:21 -0300

    first commit

---
+++ src/teste.ts
@@ -0,0 +1,6 @@
+const multiplicar = (a: number, b: number): number => a * b;
+
+const saudar = (nome: string): string => `Olá ${nome}`;
+
+console.log(multiplicar(5, 4)); 
+console.log(saudar("Maria")); 
